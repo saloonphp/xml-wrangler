0c94087b9c21f114ada57bdf98a540b06b2f3bb9
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-10-29 17:32:45 +0000

    wip

--- src/XmlReader.php
+++ src/XmlReader.php
@@ -9,6 +9,9 @@ use Exception;
 use DOMElement;
 use DOMDocument;
 use InvalidArgumentException;
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\ResponseInterface;
+use Saloon\Http\Response;
 use VeeWee\Xml\Reader\Reader;
 use VeeWee\Xml\Reader\Matcher;
 use Saloon\XmlWrangler\Data\Element;
@@ -96,6 +99,51 @@ class XmlReader
             reader: Reader::fromXmlFile(stream_get_meta_data($temporaryFile)['uri']),
             streamFile: $temporaryFile,
         );
+    }
+
+    /**
+     * Create a reader from a PSR response
+     *
+     * @param \Psr\Http\Message\MessageInterface $response
+     * @return static
+     * @throws \Saloon\XmlWrangler\Exceptions\XmlReaderException
+     */
+    public static function fromPsrResponse(MessageInterface $response): static
+    {
+        $stream = $response->getBody();
+
+        if (! $stream->isReadable()) {
+            throw new XmlReaderException('Unable to read from the stream.');
+        }
+
+        $temporaryFile = tmpfile();
+
+        if ($temporaryFile === false) {
+            throw new XmlReaderException('Unable to create the temporary file.');
+        }
+
+        while (! $stream->eof()) {
+            if ($bytes = $stream->read(1024)) {
+                fwrite($temporaryFile, $bytes);
+            }
+        }
+
+        rewind($temporaryFile);
+
+        return new static(
+            reader: Reader::fromXmlFile(stream_get_meta_data($temporaryFile)['uri']),
+            streamFile: $temporaryFile,
+        );
+    }
+
+    /**
+     * @param \Saloon\Http\Response $response
+     * @return static
+     * @throws \Saloon\XmlWrangler\Exceptions\XmlReaderException
+     */
+    public static function fromSaloonResponse(Response $response): static
+    {
+        return static::fromPsrResponse($response->getPsrResponse());
     }
 
     /**
