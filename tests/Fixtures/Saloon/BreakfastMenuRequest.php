e698c98eda5554e0cf53e9920c6e73b806a8bda3
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-10-30 20:31:42 +0000

    Fixed missing fixtures

---
+++ tests/Fixtures/Saloon/BreakfastMenuRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Saloon\XmlWrangler\Tests\Fixtures\Saloon;
+
+use Saloon\Enums\Method;
+use Saloon\Http\SoloRequest;
+
+class BreakfastMenuRequest extends SoloRequest
+{
+    protected Method $method = Method::GET;
+
+    public function resolveEndpoint(): string
+    {
+        return 'https://tests.saloon.dev/api/breakfast-menu';
+    }
+}
