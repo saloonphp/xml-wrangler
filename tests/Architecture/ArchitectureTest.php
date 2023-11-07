bfeb906c218605c433dc75f0728a47643b01f0e3
Ash Allen <mail@ashallendesign.co.uk>
2023-11-07 23:43:54 +0000

    Removed tests that break the testsuite.

--- tests/Architecture/ArchitectureTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('globals are not used')
-    ->expect(['dd', 'dump', 'ray', 'sleep', 'ddd', 'die', 'exit'])
-    ->not->toBeUsed();
-
-test('strict types')
-    ->expect('Saloon\XmlWrangler')
-    ->toUseStrictTypes();
